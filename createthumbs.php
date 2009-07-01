a6b3a3b96caac43b071ec2b872092dcf775fe6e8
www-data <www-data@claury.dyndns.org>
2009-07-01 15:57:01 +0200

    - fixed bug in locking code

--- createthumbs.php
+++ createthumbs.php
@@ -1,11 +1,11 @@
 <?php
 require "common.php";
 
-$lockfile  = "/tmp/gallerie_create_thumbs.lock";
+$lockfile  = "./gallerie_create_thumbs.lock";
 $thumbsize = 150;
 
-if (file_exists($lockfile) {
-  if (filemtime($lockfile) > (time() - 7200) {
+if (file_exists($lockfile)) {
+  if (filemtime($lockfile) < (time() - 7200)) {
     touch($lockfile);
     createThumbs($argv[1], $argv[2], $thumbsize);
     unlink($lockfile);
