b96a55aca5f13f5ae144ff69ea5713a919db9e73
Cyril Laury <cyrlaury@par-hq1.dibcom.com>
2011-03-14 14:23:34 +0100

    [resize.php] This file now defines methods for handling resized objects.

--- resize.php
+++ resize.php
@@ -1,63 +1,110 @@
 <?php
-require_once "config.php";
-require_once "common.php";
+require_once "common_db.php";
 
-$file = "";
-$dir  = "";
-if ((isset($_REQUEST['file'])) and (isset($_REQUEST['dir']))) {
-	$file = $_REQUEST['file'];
-	$dir  = $_REQUEST['dir'];	
-} else {
-	header('HTTP/1.1 400 Bad Request');
-	die('file and/or dir was not specified');
+function getObjectResizedPath($id)
+{
+    $resized = "";
+    $p_id    = -1;
+    $m_db    = new mediaDB();
+    $result  = $m_db->querySingle("SELECT folder_id, thumbnail FROM media_objects WHERE id=$id;", true);
+
+    if ($result === false) throw new Exception($m_db->lastErrorMsg());
+    $resized = $result['thumbnail'];
+    $p_id    = $result['folder_id'];
+    while($p_id != -1) {
+        $result = $m_db->querySingle("SELECT parent_id, foldername FROM media_folders WHERE id=$p_id;", true);
+        if ($result === false) throw new Exception($m_db->lastErrorMsg());
+        if ($result['foldername'] != "")
+            $resized = $result['foldername'].'/'.$resized;
+        $p_id  = $result['parent_id'];
+    }
+    return $resized;
 }
 
-global $enable_otf_gen;
-global $image_folder;
-global $resize_folder;
+function updateObjectResized($id)
+{
+    global $resized_size;
+    global $resized_folder;
+    global $image_folder;
 
-$info = pathinfo("$image_folder/$dir/$file");
-$fname_noext = $info['filename'];
-// Fix for php < 5.2
-if ($fname_noext == "" ) {
-    $fname_noext = substr($info['basename'], 0, strlen($info['basename'])-4);
+    $filename  = "";
+    $resized   = "";
+    $type      = "";
+    $lastmod   = "";
+    $p_id      = -1;
+
+    $m_db = new mediaDB();
+
+    set_time_limit(30); // Set time limit to avoid timeout
+
+    // Get Object info
+    $result = $m_db->querySingle("SELECT folder_id, thumbnail, filename, type, lastmod FROM media_objects WHERE id=$id;", true);
+    if ($result === false) throw new Exception($m_db->lastErrorMsg());
+    if ($result['thumbnail'] == "") return false; // Should never happen
+    $filename = $result['filename'];
+    $resized  = $result['thumbnail'];
+    $type     = $result['type'];
+    $lastmod  = $result['lastmod'];
+    $p_id     = $result['folder_id'];
+    // Retreive full path
+    while($p_id != -1) {
+        $result = $m_db->querySingle("SELECT parent_id, foldername FROM media_folders WHERE id=$p_id;", true);
+        if ($result === false) throw new Exception($m_db->lastErrorMsg());
+        if ($result['foldername'] != "") {
+            $resized  = $result['foldername'].'/'.$resized;
+            $filename = $result['foldername'].'/'.$filename;
+        }
+        $p_id  = $result['parent_id'];
+    }
+    $filename = $image_folder.'/'.$filename;
+    $resized  = $thumb_folder.'/'.$resized;
+
+    if ($type == 'movie') {
+        $info    = pathinfo($resized);
+        $resized = $info['dirname'].'/'.$info['filename'].'.flv';
+    }
+
+    if (file_exists($resized) && (filemtime($resized) > filemtime($filename))) return false; // No need to update
+
+    if ($type == 'picture') {
+        // Create picture resized
+        // load image and get image size
+        if (!extension_loaded('imagick')) die("-E-   php_imagick extension is required !\n");
+        if (getenv('MAGICK_THREAD_LIMIT') == "") die("-E-   This script requires the MAGICK_THREAD_LIMIT=1 line to be added in /etc/environment !\n");
+        $img    = new Imagick();
+        $img->ReadImage($filename);
+        $width  = $img->GetImageWidth();
+        $height = $img->GetImageHeight();
+        // Compute resized size
+        if ($width >= $height) {
+            $orientation = 0;
+            $ratio = $width / $height;
+        } else {
+            $orientation = 1;
+            $ratio = $height / $width;
+        }
+
+        $new_width = $resize_width;
+        if ($orientation == 0) {
+            $new_height = $new_width / $ratio;
+        } else {
+            $new_height = $new_width;
+            $new_width  = $new_height / $ratio;
+        }
+        $img->thumbnailImage($new_width, $new_height);
+        $img->setImageFormat("jpeg");
+        $img->setCompressionQuality(65);
+        $img->setImageFilename($resized);
+        $img->WriteImage();
+        $img->clear();
+        $img->destroy();
+    } else {
+        // Create video thumbnail
+        exec("mencoder \"$filename\" -o \"$resized\" -quiet -of lavf -oac mp3lame -lameopts abr:br=64:mode=3 -ovc lavc -lavcopts vcodec=flv:vbitrate=1600:mbd=2:mv0:trell:v4mv:cbp:last_pred=4 -ofps 15 -srate 44100");
+    }
+
+    $m_db->close();
+    return true;
 }
-
-$resize = $resize_folder.'/'.$dir.'/'.$fname_noext.'.jpg';
-
-if (!file_exists($resize))
-    if ($enable_otf_gen == 1)
-        createResize($dir, $file);
-    else
-        $resize = './images/nothumb.jpg';
-else if (filemtime($resize) < filemtime("$image_folder/$dir/$file"))
-    if ($enable_otf_gen == 1)
-        createResize($dir, $file);
-
-$gmdate_mod = gmdate("D, d M Y H:i:s", filemtime($resize));
-if(!strstr($gmdate_mod, "GMT")) {
-	$gmdate_mod .= " GMT";
-}
-if (isset($_SERVER["HTTP_IF_MODIFIED_SINCE"])) {
-	// check for updates
-	$if_modified_since = preg_replace ("/;.*$/", "", $_SERVER["HTTP_IF_MODIFIED_SINCE"]);
-	if ($if_modified_since == $gmdate_mod) {
-		header("HTTP/1.1 304 Not Modified");
-		die();
-	}
-}
-
-$fileSize = filesize($resize);
-
-// send headers then display image
-header ('Content-Type: image/jpeg');
-header ('Accept-Ranges: bytes');
-header ('Last-Modified: ' . $gmdate_mod);
-header ('Content-Length: ' . $fileSize);
-header ('Cache-Control: max-age=3600, must-revalidate');
-header ('Expires: ' . $gmdate_mod);
-
-readfile ($resize);
-die();
 
 ?>
