dd0d70f953c4dc18e7954b6072f7c65e0109b1e1
Cyril Laury <cyrlaury@par-hq1.dibcom.com>
2011-03-16 10:50:59 +0100

    [update_gallery] Rewrite queries to close them ASAP

--- update_gallery.php
+++ update_gallery.php
@@ -65,10 +65,16 @@ echo "Found $element_count elements and $folder_count folders to process\n";
 
 // Clean and create folders
 echo "Processing folders...\n";
-$folder_list = $gallery_db->query("SELECT id FROM media_folders;");
-if($folder_list === false) throw new Exception ($gallery_db->lastErrorMsg());
-while($folder = $folder_list->fetchArray()) {
-    $folder_path = $gallery_db->getFolderPath($folder['id']);
+$folder_list = array();
+$folder_list_db = $gallery_db->query("SELECT id FROM media_folders;");
+if($folder_list_db === false) throw new Exception ($gallery_db->lastErrorMsg());
+while($folder = $folder_list_db->fetchArray(SQLITE3_ASSOC)) {
+    $folder_list[] = $folder['id'];
+}
+$folder_list_db->finalize();
+
+foreach($folder_list as $folder_id) {
+    $folder_path = $gallery_db->getFolderPath($folder_id);
     echo "-D- processing $folder_path \n";
     if (!file_exists("$thumb_folder/$folder_path")) { 
         mkdir("$thumb_folder/$folder_path", 0777, true);
@@ -76,7 +82,16 @@ while($folder = $folder_list->fetchArray()) {
     if (!file_exists("$resized_folder/$folder_path")) {
         mkdir("$resized_folder/$folder_path", 0777, true); 
     }
-    updateFolderThumbnail($folder['id']);
+    updateFolderThumbnail($folder_id);
+
+    // build folder thumbnail list
+    $thumbnail_list = array();
+    $thumbnail_list_db = $gallery_db->query("SELECT id FROM media_objects WHERE folder_id=$folder_id;");
+    if ($thumbnail_list_db === false) throw new Exception($gallery_db->lastErrorMsg());
+    while($thumbnail_db = $thumbnail_list_db->fetchArray(SQLITE3_ASSOC)) {
+        $thumbnail_list[] = getThumbnailPath($thumbnail_db['id']);
+    }
+    $thumbnail_list_db->finalize();
 
     // open the thumbnail directory
     $dir = opendir("$thumb_folder/$folder_path");
@@ -88,10 +103,7 @@ while($folder = $folder_list->fetchArray()) {
             if ($fname == $folder_thumbname) continue;
             // Check if file exist
             $found = false;
-            $element_list = $gallery_db->query("SELECT id FROM media_objects WHERE folder_id=".$folder['id']);
-            if ($element_list === false) throw new Exception ($gallery_db->lastErrorMsg());
-            while($element = $element_list->fetchArray()) {
-                $thumbnail_path = getThumbnailPath($element['id'], $gallery_db);
+            foreach($thumbnail_list as $thumbnail_path) {
                 if (stristr($thumbnail_path, "$folder_path/$fname") !== false)
                     $found = true;
                 else if (($folder_path == "") && (stristr($thumbnail_path, "$fname") !== false))
@@ -106,6 +118,15 @@ while($folder = $folder_list->fetchArray()) {
     }
     closedir($dir);
 
+    // build folder reszied list
+    $resized_list = array();
+    $resized_list_db = $gallery_db->query("SELECT id FROM media_objects WHERE folder_id=$folder_id;");
+    if ($resized_list_db === false) throw new Exception($gallery_db->lastErrorMsg());
+    while($resized_db = $resized_list_db->fetchArray(SQLITE3_ASSOC)) {
+        $resized_list[] = getResizedPath($resized_db['id']);
+    }
+    $resized_list_db->finalize();
+
     // open the resized directory
     $dir = opendir("$resized_folder/$folder_path");
 
@@ -114,11 +135,8 @@ while($folder = $folder_list->fetchArray()) {
         if (is_file("$resized_folder/$folder_path/$fname")) {
             // Check if file exist
             $found = false;
-            $element_list = $gallery_db->query("SELECT id FROM media_objects WHERE folder_id=".$folder['id']);
-            if ($element_list === false) throw new Exception ($gallery_db->lastErrorMsg());
-            while($element = $element_list->fetchArray()) {
+            foreach($resized_list as $resized_path) {
                 $fname_noext  = substr($fname, 0, strlen($fname) - 3);
-                $resized_path = getResizedPath($element['id'], $gallery_db);
                 if (stristr($resized_path, "$folder_path/$fname_noext") !== false)
                     $found = true;
                 else if (($folder_path == "") && (stristr($resized_path, "$fname_noext") !== false))
