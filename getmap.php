65ec8761030a8e2c621066294cc401c8f6fe7261
Cyril Laury <cyrlaury@par-hq1.dibcom.com>
2011-03-17 16:21:16 +0100

    [maps] Polished style. Fixed marker icons selection. Maps now fit the whole folder.

--- getmap.php
+++ getmap.php
@@ -33,19 +33,26 @@ $elements_list = getFolderGeolocalizedElements($id, $m_db);
     $(document).ready(function(){
 <?php
 $element = new mediaObject();
-$m_db->loadMediaObject($element, $elements_list[0]);
-echo "    var latlng = new google.maps.LatLng($element->latitude, $element->longitude);\n";
+echo "    var latlng = new google.maps.LatLng(0,0);\n";
 ?>
     var myOptions = {
-        zoom: 14,
+        zoom: 18,
         center: latlng,
         mapTypeId: google.maps.MapTypeId.SATELLITE
     };
     var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 <?php
+// Build map bound
+$map_bounds = getFolderGeolocalizedBounds($id, $m_db);
+echo "     var latlng_ne = new google.maps.LatLng(".$map_bounds['ne_lat'].",".$map_bounds['ne_lon'].");\n";
+echo "     var latlng_sw = new google.maps.LatLng(".$map_bounds['sw_lat'].",".$map_bounds['sw_lon'].");\n";
+echo "     var latlng_bound = new google.maps.LatLngBounds(latlng_sw, latlng_ne);\n";
+echo "     map.fitBounds(latlng_bound);\n";
+// Build element markers/info
 $infoid = 0;
 foreach($elements_list as $element_id) {
     $m_db->loadMediaObject($element, $element_id);
+    if ($element->type == 'video') continue; // Videos are not geotagged
     echo "var LatLng$infoid = new google.maps.LatLng($element->latitude, $element->longitude);\n";
     echo "var contentString$infoid = '<div id=\"map_info\">'+\n";
     echo "\t'<h2>".$element->title."</h2>'+\n";
@@ -53,7 +60,7 @@ foreach($elements_list as $element_id) {
     echo "\t'<img src=\"./getthumb.php?id=$element_id\" alt=\"".$element->filename."\"/>'+\n";
     echo "\t'<p>Alt: ".round($element->altitude)."m<br />".$element->getSubTitle(true)."</p>'+\n";
     echo "\t'</a></div>';\n";
-    echo "var marker$infoid = new google.maps.Marker({ position: LatLng$infoid, map: map, title:'".$element->title."', icon:'".getElementIcon($element)."' });\n";
+    echo "var marker$infoid = new google.maps.Marker({ position: LatLng$infoid, map: map, title:'".$element->title."', icon:'".getElementIcon($element_id, $m_db)."' });\n";
     echo "var infooptions$infoid = { content: contentString$infoid, alignBottom: true, pixelOffset: new google.maps.Size(0, -35), boxClass: \"map_info\", closeBoxMargin: \"5px\" };\n";
     echo "var infobox$infoid = new InfoBox(infooptions$infoid);\n";
     echo "google.maps.event.addListener(marker$infoid, 'click', function() { infobox$infoid.open(map, marker$infoid); });\n";
--- googlemaps.php
+++ googlemaps.php
@@ -39,25 +39,76 @@ function getFolderGeolocalizedElements($id, mediaDB &$db = NULL)
     return $element_list;
 }
 
-function getElementIcon(mediaObject $element)
+function getElementIcon($id, mediaDB &$db = NULL)
 {
-    foreach($element->tags as $tag) {
-        if      (stristr($tag, 'Paysage') != false)
-            return 'images/markers/paysage.png';
-        else if (stristr($tag, 'Flore')   != false)
-            return 'images/markers/flore.png';
-        else if (stristr($tag, 'Faune')   != false)
-            return 'images/markers/faune.png';
-        else if (stristr($tag, 'Eglise')  != false)
-            return 'images/markers/eglise.png';
-        else if (stristr($tag, 'Pano')    != false)
-            return 'images/markers/panorama.png';
-        else if (stristr($tag, 'Portrait') != false)
-            return 'images/markers/portrait.png';
-    }
-    if ($element->type == 'movie')
-        return 'images/markers/movie.png';
+    $result = '';
+
+    $m_db = NULL;
+    if ($db == NULL)
+        $m_db = new mediaDB();
     else
-        return 'images/markers/picture.png';
+        $m_db = $db;
+
+    $tags = $m_db->query("SELECT name FROM media_tags WHERE media_id=$id;");
+    if ($tags === false) throw new Exception($m_db->lastErrorMsg());
+
+    while($tag = $tags->fetchArray()) {
+        if      (stristr($tag['name'], 'Paysage') != false) {
+            $result = 'images/markers/paysage.png';
+            break;
+        } else if (stristr($tag['name'], 'Flore')   != false) {
+            $result = 'images/markers/flore.png';
+            break;
+        } else if (stristr($tag['name'], 'Faune')   != false) {
+            $result = 'images/markers/faune.png';
+            break;
+        } else if (stristr($tag['name'], 'Eglise')  != false) {
+            $result = 'images/markers/eglise.png';
+            break;
+        } else if (stristr($tag['name'], 'Pano')    != false) {
+            $result = 'images/markers/panorama.png';
+            break;
+        } else if (stristr($tag['name'], 'Portrait') != false) {
+            $result = 'images/markers/portrait.png';
+            break;
+        }
+    }
+    $tags->finalize();
+
+    if ($result == '')
+        $result = 'images/markers/picture.png';
+
+    if ($db == NULL)
+        $m_db->close();
+
+    return $result;
 }
+
+function getFolderGeolocalizedBounds($id, mediaDB &$db = NULL)
+{    
+    $m_db          = NULL;
+    $longitude_min =  180.0;
+    $longitude_max = -180.0;
+    $latitude_min  =   90.0;
+    $latitude_max  =  -90.0;
+
+    if ($db == NULL) $m_db = new mediaDB();
+    else             $m_db = $db;
+
+    $results = $m_db->query("SELECT latitude, longitude FROM media_objects WHERE folder_id=$id AND longitude != 9999.0;");
+    if ($results === false) throw new Exception($m_db->lastErrorMsg());
+    while($row = $results->fetchArray()) {
+        if ($row['longitude'] < $longitude_min) $longitude_min = $row['longitude'];
+        if ($row['longitude'] > $longitude_max) $longitude_max = $row['longitude'];
+        if ($row['latitude']  < $latitude_min)  $latitude_min  = $row['latitude'];
+        if ($row['latitude']  > $latitude_max)  $latitude_max  = $row['latitude'];
+    }
+    $results->finalize();
+
+    if ($db == NULL)
+        $m_db->close();
+
+    return array('sw_lat' => $latitude_min, 'sw_lon' => $longitude_min, 'ne_lat' => $latitude_max, 'ne_lon' => $longitude_max);
+}
+
 ?>
