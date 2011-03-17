ad644f1fccfcc29307946f5e61b2e75876dc817c
Cyril Laury <cyrlaury@par-hq1.dibcom.com>
2011-03-17 12:09:22 +0100

    [getmap] Now use same parameters as index.php (drop-in replace). Integrated map in the look'n'feel of the site.

--- getmap.php
+++ getmap.php
@@ -1,38 +1,74 @@
 <?php
+require_once "common_browser.php";
 require_once "googlemaps.php";
 
-if (isset($_REQUEST['id'])) {
-    $id = $_REQUEST['id'];
-} else {
-	header('HTTP/1.1 400 Bad Request');
-	die('id was not specified');
-}
+$starttime = explode(' ', microtime());
+$starttime = $starttime[1] + $starttime[0];
 
-$m_db = new mediaDB();
+if (isset($_GET['path']))
+    $path = $_GET["path"];
+else {
+	header('HTTP/1.1 400 Bad Request');
+	die('path was not specified');
+    
+}
+$path = safeDirectory($path);
+
+$m_db          = new mediaDB();
+$id            = $m_db->getFolderID($path);
 $elements_list = getFolderGeolocalizedElements($id, $m_db);
-echo "<!DOCTYPE html>\n";
-echo "<html>\n";
-echo "<head>\n";
-echo "<meta name=\"viewport\" content=\"initial-scale=1.0, user-scalable=no\" />\n";
-echo "<style type=\"text/css\">\n";
-echo "  html { height: 100% }\n";
-echo "  body { height: 100%; margin: 0px; padding: 0px }\n";
-echo "  #map_canvas { height: 100% }\n";
-echo "</style>\n";
-echo "<script type=\"text/javascript\"\n";
-echo "    src=\"http://maps.google.com/maps/api/js?sensor=false\">\n";
-echo "</script>\n";
-echo "<script type=\"text/javascript\">\n";
-echo "  function initialize() {\n";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<?php echo "<title>".$gal_title."</title>\n"; ?> 
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+  <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+  <link rel="stylesheet" href="css/layout.css" type="text/css" media="screen" charset="utf-8" />
+  <link rel="stylesheet" href="css/prettyPhoto.css" type="text/css" media="screen" charset="utf-8" /> 
+  <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+  <script src="js/jquery-1.4.2.min.js" type="text/javascript"></script>		
+  <script src="js/jquery.tools-1.2.1.min.js" type="text/javascript"></script>
+  <script src="js/flowplayer-3.2.2.min.js" type="text/javascript"></script>
+  <script src="js/jquery.prettyPhoto.js" type="text/javascript" charset="utf-8"></script> 
+  <script type="text/javascript" charset="utf-8">
+    $(document).ready(function(){
+      $("a[rel^='prettyPhoto']").prettyPhoto({
+        animationSpeed: 'fast',
+    	padding: 30,
+	    opacity: 0.65,
+	    showTitle: true,
+	    allowresize: true,
+	    counter_separator_label: '/',
+	    theme: '<?php echo $gal_theme; ?>' 
+      });
+    });
+    $(function() {
+      $("a[rel^='#video']").overlay({
+         expose: '#111',
+         effect: 'apple',
+         onLoad: function(content) {
+            this.getOverlay.find("a.player").flowplayer(0).load();
+         }
+      });
+      $("a.player").flowplayer("./swf/flowplayer-3.2.2.swf", { clip: { scaling: 'fit' } });
+    });
+/*    $(function() {
+      $("#map_canvas").height($(document).height() * 0.6);
+    });*/
+    $(function() {
+<?php
 $element = new mediaObject();
 $m_db->loadMediaObject($element, $elements_list[0]);
 echo "    var latlng = new google.maps.LatLng($element->latitude, $element->longitude);\n";
-echo "    var myOptions = {\n";
-echo "      zoom: 14,\n";
-echo "      center: latlng,\n";
-echo "      mapTypeId: google.maps.MapTypeId.SATELLITE\n";
-echo "    };\n";
-echo "    var map = new google.maps.Map(document.getElementById(\"map_canvas\"), myOptions);\n";
+?>
+    var myOptions = {
+        zoom: 14,
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.SATELLITE
+    };
+    var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+<?php
 $infoid = 0;
 foreach($elements_list as $element_id) {
     $m_db->loadMediaObject($element, $element_id);
@@ -48,11 +84,32 @@ foreach($elements_list as $element_id) {
     echo "google.maps.event.addListener(marker$infoid, 'click', function() { infowindow$infoid.open(map, marker$infoid); });\n";
     $infoid++;
 }
-echo "  }\n";
-echo "</script>\n";
-echo "</head>\n";
-echo "<body onload=\"initialize()\">\n";
-echo "  <div id=\"map_canvas\" style=\"width:100%; height:100%\"></div>\n";
-echo "</body>\n";
-echo "</html>\n";
 ?>
+    });
+</script>
+</head>
+<body>
+<?php
+displayTopFoldersMenu($m_db);
+echo "<div id=\"content\">\n"; 
+echo "<h1>".htmlentities($gal_title)."</h1>\n"; 
+
+// Path dirs and link
+displayFolderHierarchy($id, $m_db, false);
+?>
+<div id="map_canvas"></div>
+<div class="clearfix"></div> 
+<br/>
+<?php
+$mtime = explode(' ', microtime());
+$totaltime = $mtime[0] + $mtime[1] - $starttime;
+$today = date('Y/m/d \a\t H:i:s');
+printf('Page generated in %.3f seconds on %s', $totaltime, $today);
+?>
+</div>
+<ul class="submenu">
+<li>Gallerie v2.0.0 - H. Raffard &amp; C. Laury - 2011/03/15</li>
+</ul>
+<br clear="all" /> 
+</body>
+</html>
