4408b83b2dd6509b5553604f5a396d2a3fcb1607
Cyril Laury <cyrlaury@par-hq1.dibcom.com>
2011-03-29 11:19:35 +0200

    [update_status] filter notices about session GC

--- update_status.php
+++ update_status.php
@@ -1,10 +1,10 @@
 <?php
 
 if (isset($_REQUEST['status'])) {
-    session_start();
+    @session_start();
     echo $_SESSION['status'];
 } else {
-    session_start();
+    @session_start();
     echo $_SESSION['progress'];
 }
 
