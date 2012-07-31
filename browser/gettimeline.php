318f82c9f84910d7807f5c0a151930f6a4e46e3f
Cyril Laury <cyril.laury@laposte.net>
2012-07-31 10:01:45 +0200

    Improved display of timeline

--- browser/gettimeline.php
+++ browser/gettimeline.php
@@ -29,25 +29,26 @@ if ((file_exists($json) === FALSE ) || (filemtime("$BASE_DIR/cache/timeline-1.js
   <script src="<?php echo $BASE_URL?>/js/navigation.js" type="text/javascript"></script>
 </head>
 <body>
+<div id="timeline_content">
 <?php
 echo "<h1><a href=\"$BASE_URL/index.php\">".htmlentities($gal_title)."</a></h1>\n"; 
 // Path dirs and link
 displayFolderHierarchy($top_id, $m_db, false);
 ?>
 <div id="timeline-embed"></div>
-  <script type="text/javascript">
-    var timeline_config = {
-        width:  "100%",
-        height: "90%",
-        source: '<?php echo $jurl?>',
-        lang:   'fr',
-        <?php if ($top_id != 1) echo "start_at_end: true,\n"; ?>
-        // hash_bookmark: true,       
-        css:    '<?php echo $BASE_URL?>/css/timeline-dark.css',
-        js:     '<?php echo $BASE_URL?>/js/timeline-min.js'   
-    }
-  </script>
-  <script type="text/javascript" src="<?php echo $BASE_URL?>/js/timeline-embed.js"></script>
-
+<script type="text/javascript">
+  var timeline_config = {
+      width:  "100%",
+      height: "90%",
+      source: '<?php echo $jurl?>',
+      lang:   'fr',
+      <?php if ($top_id != 1) echo "start_at_end: true,\n"; ?>
+      // hash_bookmark: true,       
+      css:    '<?php echo $BASE_URL?>/css/timeline-dark.css',
+      js:     '<?php echo $BASE_URL?>/js/timeline-min.js'   
+  }
+</script>
+<script type="text/javascript" src="<?php echo $BASE_URL?>/js/timeline-embed.js"></script>
+</div>
 </body>
 </html>
--- common/tools.php
+++ common/tools.php
@@ -104,7 +104,10 @@ function genTimelineData(mediaDB &$db = NULL, $top_id = 1)
             $json_data .= "\t\t\t\t".'"asset":'."\n";
             $json_data .= "\t\t\t\t\t{\n";
             $json_data .= "\t\t\t\t\t\t".'"media":"'."$BASE_URL/$resized_folder/".getResizedPath($itemid).'",'."\n";
-            $json_data .= "\t\t\t\t\t\t".'"credit":"",'."\n";
+            if ($element->altitude > -9999.0)
+                $json_data .= "\t\t\t\t\t\t".'"credit":"'.round($element->altitude).'m",'."\n";
+            else
+                $json_data .= "\t\t\t\t\t\t".'"credit":"",'."\n";
             $json_data .= "\t\t\t\t\t\t".'"thumbnail":"'."$BASE_URL/$thumb_folder/".getThumbnailPath($itemid).'",'."\n";
             $json_data .= "\t\t\t\t\t\t".'"caption":"'.utf8_encode($element->getSubTitle(true)).'"'."\n";
             $json_data .= "\t\t\t\t\t}\n";
